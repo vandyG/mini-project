8a7c58ab680e53c035a3df03f4cc597b0b1f4609
vandyG <vandy.goel23@gmail.com>
2024-09-03 12:02:50 -0500

    add more features

--- problemA.py
+++ problemA.py
@@ -1,4 +1,6 @@
-"""# String /input statement /print out
+# %%
+"""
+# String /input statement /print out
 1. Print a welcome message to the user.
 2. Take a string (message) and a letter from the user.
 3. Count how many times this letter occurred.
@@ -6,16 +8,31 @@
 5. Print the outputs to the user.
 """
 
+# %%
 import argparse
 
 
+# %%
+def set_get_letter(input: str) -> str:
+    if len(input) > 1:
+        raise TypeError("Please provide a single letter as input.")
+
+    return input
+
+
+# %%
 def main():
     print("Welcome User!")
 
-    parser = argparse.ArgumentParser(prog="Problem A", description="Problem A")
-    parser.add_argument("message")
-    parser.add_argument("letter")
-    args = parser.parse_args()
+    parser = argparse.ArgumentParser(prog="problemA", description="Problem A")
+    parser.add_argument("message", default=None, nargs="?")
+    parser.add_argument("letter", default=None, nargs="?", type=set_get_letter)
+    args, unknown = parser.parse_known_args(["--f"])
+
+    if not args.message:
+        args.message = input("Please enter a message:")
+    if not args.letter:
+        args.letter = set_get_letter(input("Please enter a letter to search:"))
 
     message: str = args.message.lower()  # type: ignore
     letter: str = args.letter.lower()  # type: ignore
@@ -27,5 +44,6 @@ def main():
     print(f"Percentage: {percentage:.2%}")
 
 
+# %%
 if __name__ == "__main__":
     main()
