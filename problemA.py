1cfd3baa653cde940e25b46be392a085076dd71e
vandyG <vandy.goel23@gmail.com>
2024-09-03 02:50:49 -0500

    complete prblem a

--- problemA.py
+++ problemA.py
@@ -5,3 +5,27 @@
 4. Calculate the percentage of the letter in the message.
 5. Print the outputs to the user.
 """
+
+import argparse
+
+
+def main():
+    print("Welcome User!")
+
+    parser = argparse.ArgumentParser(prog="Problem A", description="Problem A")
+    parser.add_argument("message")
+    parser.add_argument("letter")
+    args = parser.parse_args()
+
+    message: str = args.message.lower()  # type: ignore
+    letter: str = args.letter.lower()  # type: ignore
+
+    count = message.count(letter)
+    print(f"Count: {count}")
+
+    percentage = count / len(message)
+    print(f"Percentage: {percentage:.2%}")
+
+
+if __name__ == "__main__":
+    main()
