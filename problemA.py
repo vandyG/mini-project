24cdd5558b6f01e841e431c02e6c231c6e80d662
vandyG <vandy.goel23@gmail.com>
2024-09-03 02:11:06 -0500

    add problem 1 code

---
+++ problemA.py
@@ -0,0 +1,7 @@
+"""# String /input statement /print out
+1. Print a welcome message to the user.
+2. Take a string (message) and a letter from the user.
+3. Count how many times this letter occurred.
+4. Calculate the percentage of the letter in the message.
+5. Print the outputs to the user.
+"""
